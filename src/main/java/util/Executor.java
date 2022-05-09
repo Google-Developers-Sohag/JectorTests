c1162a72faf2448d8392cbee0128f52e9d4b8a7a
Scrappers <scrappers.tm@gmail.com>
2022-05-09 12:23:55 +0200

    Added recursive funtion for reflective package executor

--- src/main/java/util/Executor.java
+++ src/main/java/util/Executor.java
@@ -1,7 +1,5 @@
 package util;
 
-import test.core.UnitTest;
-
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -13,20 +11,19 @@ import java.util.logging.Logger;
  */
 public final class Executor {
     private static final Logger logger = Logger.getLogger(Executor.class.getName());
-
+    private static final String fileSeparator = "/";
     private static Executor executor;
-    private final String package_;
+    private String method = "";
+    private Object params;
 
-
-    private Executor(final String package_) {
-        this.package_ = package_;
+    private Executor() {
     }
 
-    public static Executor getInstance(final String package_) {
+    public static Executor getInstance() {
         if (executor == null) {
             synchronized (Executor.class) {
                 if (executor == null) {
-                    executor = new Executor(package_);
+                    executor = new Executor();
                     logger.log(Level.INFO, "Prepared the executor instance.");
                 }
             }
@@ -34,30 +31,72 @@ public final class Executor {
         return executor;
     }
 
-    public void executeMemberClasses() throws IllegalAccessException,
+    public void runJavaClasses(final String package_) throws IllegalAccessException,
             InstantiationException,
-            NoSuchMethodException,
             InvocationTargetException, ClassNotFoundException {
-        final File file = new File(Executor.this.getClass().getResource("/" + package_).getFile());
-        final String[] filePaths = file.list();
-        if (filePaths != null) {
-            for (String path : filePaths) {
-                // don't try to invoke non-runnable files and subclasses
-                if (path.contains(".class") && (!path.contains("$"))) {
-                    path = path.substring(0, path.length() - (".class").length());
-                    path = package_ + "." + path;
-                    final Class<?> clazz = Class.forName(path);
-                    // invoke only the unit test classes
-                    if (clazz.newInstance() instanceof UnitTest) {
-                        final Method method = clazz.getDeclaredMethod("runTest");
-                        method.setAccessible(true);
-                        method.invoke(clazz.newInstance());
-                        logger.log(Level.INFO, clazz.getName() + " test invoked.");
-                    } else {
-                        logger.log(Level.WARNING, clazz.getName() + " is not a unit test.");
-                    }
-                }
+        final String[] files = openPackage(package_).list();
+        if (files == null) {
+            return;
+        }
+        for (String file : files) {
+            if (openPackage(buildFromPackage(package_, file)).list() != null) {
+                Executor.getInstance().runJavaClasses(buildFromPackage(package_, file));
+            } else {
+                execute(package_.replaceFirst(Executor.fileSeparator, "")
+                        .replaceAll(Executor.fileSeparator, "."), file);
             }
         }
+    }
+
+    private File openPackage(final String package_) {
+        return new File(Executor.this.getClass().getResource(package_).getFile());
+    }
+
+    private String buildFromPackage(final String package_, final String file) {
+        return package_ + Executor.fileSeparator + file;
+    }
+
+    private void execute(final String package_, String clazzName) throws IllegalAccessException,
+            InstantiationException, ClassNotFoundException, InvocationTargetException {
+        // don't try to invoke non-runnable files and subclasses
+        if (clazzName.contains(".class") && (!clazzName.contains("$"))) {
+            clazzName = clazzName.substring(0, clazzName.length() - (".class").length());
+            clazzName = package_ + "." + clazzName;
+            final Class<?> clazz = Class.forName(clazzName);
+            // invoke only the unit test classes
+            try {
+                final Method method = getMethod(clazz, executor.method, params);
+                method.setAccessible(true);
+                invokeMethod(clazz, method, params);
+                logger.log(Level.INFO, clazz.getName() + " method " + executor.method + " invoked.");
+            } catch (final NoSuchMethodException exception) {
+                logger.log(Level.WARNING, clazz.getName() + " doesn't have the method " + executor.method);
+            } catch (final InstantiationException exception2) {
+                logger.log(Level.WARNING, clazz.getName() + " is an abstract class.");
+            }
+        }
+    }
+
+    public void setMethod(String method, Object params) {
+        this.method = method;
+        this.params = params;
+    }
+
+    private Method getMethod(final Class<?> clazz,
+                             final String method, final Object params)
+            throws NoSuchMethodException {
+        if (params == null) {
+            return clazz.getDeclaredMethod(method);
+        }
+        return clazz.getDeclaredMethod(method, params.getClass());
+    }
+
+    private Object invokeMethod(final Class<?> clazz,
+                                final Method method, final Object params) throws
+            IllegalAccessException, InstantiationException, InvocationTargetException {
+        if (params == null) {
+            return method.invoke(clazz.newInstance());
+        }
+        return method.invoke(clazz.newInstance(), params);
     }
 }
--- src/main/java/util/Launcher.java
+++ src/main/java/util/Launcher.java
@@ -3,7 +3,9 @@ package util;
 import java.lang.reflect.InvocationTargetException;
 
 public final class Launcher {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        Executor.getInstance("test").executeMemberClasses();
+    public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        final Executor executor = Executor.getInstance();
+        executor.setMethod("runTest", null);
+        executor.runJavaClasses("/" + "test");
     }
 }
